4c210a56f18759900de99ce25cbf84b942c3f0a0
keyakimichi <shinjava0@hotmail.com>
2018-08-07 07:05:56 +0900

    エコーアプリケーション実装

---
+++ src/main/java/org/seasar/sastruts/example/action/EchoAction.java
@@ -0,0 +1,26 @@
+package org.seasar.sastruts.example.action;
+
+import javax.annotation.Resource;
+
+import org.seasar.sastruts.example.form.EchoForm;
+import org.seasar.struts.annotation.ActionForm;
+import org.seasar.struts.annotation.Execute;
+
+public class EchoAction {
+	/** アクションフォーム */
+	@Resource
+	@ActionForm
+	public EchoForm echoForm;
+
+	/** 入力画面の実行メソッド */
+	@Execute(validator=false)
+	public String index() {
+		return "input.jsp";
+	}
+
+	/** エコー画面の実行メソッド */
+	@Execute(validator=false)
+	public String echo() {
+		return "echo.jsp";
+	}
+}
---
+++ src/main/java/org/seasar/sastruts/example/form/EchoForm.java
@@ -0,0 +1,5 @@
+package org.seasar.sastruts.example.form;
+
+public class EchoForm {
+	public String name;
+}
